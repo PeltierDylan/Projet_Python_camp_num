4bcf98296170e8afed66fc5e0c47b4287eb90791
PeltierDylan <dylan.peltier.dp@gmail.com>
2023-11-09 11:34:01 +0100

    Edit: projet.py

--- projet.py
+++ projet.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urlparse
 
-
+# etape 1
 def compter_occurrences(texte):
     mots = re.findall(r'\b\w+\b', texte.lower())
     occurrences = Counter(mots)
@@ -17,66 +17,14 @@ def retirer_parasites(occurrences, parasites):
     return {mot: occurrences[mot] for mot in occurrences if mot.lower() not in parasites}
 
 
-# Etape 3
+# etape 3
 def recuperer_parasites(fichier):
     with open(fichier, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         mots_parasites = {row[0].lower() for row in reader}
     return mots_parasites
 
-# Etape 5
-def nettoyer_html(html):
-    soup = BeautifulSoup(html, "html.parser")
-    return soup.get_text()
-
-# Etape 6
-def extraire_attributs(html, balise, attribut):
-    soup = BeautifulSoup(html, "html.parser")
-    return [tag[attribut] for tag in soup.find_all(balise) if tag.has_attr(attribut)]
-
-
-# Etape 8
-def extraire_nom_domaine(url):
-    return urlparse(url).netloc
-
-# Etape 9
-def classifier_par_domaine(domaine, urls):
-    internes, externes = [], []
-    for url in urls:
-        if domaine in url:
-            internes.append(url)
-        else:
-            externes.append(url)
-    return internes, externes
-
-# Etape 10
-def recuperer_html(url):
-    response = requests.get(url)
-    return response.text
-
-# Etape 11
-def audit_page(url):
-    html = recuperer_html(url)
-    texte = nettoyer_html(html)
-    occurrences = compter_occurrences(texte)
-    mots_parasites = recuperer_parasites("parasites.csv")
-    mots_cles = retirer_parasites(occurrences, mots_parasites)
-    
-    print("Mots clés avec les 3 premières valeurs d'occurrences :")
-    for mot, occ in list(mots_cles.items())[:3]:
-        print(f"{mot}: {occ}")
-    
-    liens = extraire_attributs(html, 'a', 'href')
-    liens_entrants = [lien for lien in liens if lien.startswith(url)]
-    liens_sortants = [lien for lien in liens if not lien.startswith(url)]
-    
-    print(f"Nombre de liens entrants: {len(liens_entrants)}")
-    print(f"Nombre de liens sortants: {len(liens_sortants)}")
-    
-    alts = extraire_attributs(html, 'img', 'alt')
-    print(f"Présence de balises alt: {alts if alts else 'Non'}")
-
-
+# etape 4
 def test_etape4():
     print("Test de l'étape 4 :");print("******************")
     texte = "La brise d'automne disperse les feuilles colorées. Un écureuil agile saute entre les branches. Loin, une cloche d'église résonne, rythmant le crépuscule naissant. Les pensées s'évadent, légères, au gré du vent frais, tandis que le monde s'habille de nuances orangées, annonçant la nuit étoilée."
@@ -86,6 +34,17 @@ def test_etape4():
     print("Occurences des mots : ", end='');print(mots_occurrences)
     print("Occurences des mots sans parasites : ", end="");print(resultat)
 
+# Etape 5
+def nettoyer_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return soup.get_text()
+
+# etape 6
+def extraire_attributs(html, balise, attribut):
+    soup = BeautifulSoup(html, "html.parser")
+    return [tag[attribut] for tag in soup.find_all(balise) if tag.has_attr(attribut)]
+
+# Etape 7
 def test_etape7():
     print("Test de l'étape 7 :");print("******************")
     html_exemple = """
@@ -125,6 +84,47 @@ def test_etape7():
     href_values = extraire_attributs(html_exemple, 'a', 'href')
     print("\nValeurs de l'attribut 'href' :")
     print(href_values)
+
+#Etape 8
+def extraire_nom_domaine(url):
+    return urlparse(url).netloc
+
+# Etape 9
+def classifier_par_domaine(domaine, urls):
+    internes, externes = [], []
+    for url in urls:
+        if domaine in url:
+            internes.append(url)
+        else:
+            externes.append(url)
+    return internes, externes
+
+# Etape 10
+def recuperer_html(url):
+    response = requests.get(url)
+    return response.text
+
+# Etape 11
+def audit_page(url):
+    html = recuperer_html(url)
+    texte = nettoyer_html(html)
+    occurrences = compter_occurrences(texte)
+    mots_parasites = recuperer_parasites("parasites.csv")
+    mots_cles = retirer_parasites(occurrences, mots_parasites)
+    
+    print("Mots clés avec les 3 premières valeurs d'occurrences :")
+    for mot, occ in list(mots_cles.items())[:3]:
+        print(f"{mot}: {occ}")
+    
+    liens = extraire_attributs(html, 'a', 'href')
+    liens_entrants = [lien for lien in liens if lien.startswith(url)]
+    liens_sortants = [lien for lien in liens if not lien.startswith(url)]
+    
+    print(f"Nombre de liens entrants: {len(liens_entrants)}")
+    print(f"Nombre de liens sortants: {len(liens_sortants)}")
+    
+    alts = extraire_attributs(html, 'img', 'alt')
+    print(f"Présence de balises alt: {alts if alts else 'Non'}")
 
 # Exemple d'utilisation
 if __name__ == "__main__":
